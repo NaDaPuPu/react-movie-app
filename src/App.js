d8c9b6c8b398361840a0264bca9bae10429be06b
NaDaPuPu <jun19998@gmail.com>
2021-04-16 15:35:14 +0900

    Chapter 2.2.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
 import React from 'react';
-import Potato from './Potato';
 
-function Food({ fav }) {
-  return <h3>I like {fav}</h3>;
+function Food({ name, desc }) {
+  return (
+    <div>
+      <h2>I like {name}</h2>
+      <h3>{desc}</h3>
+    </div>
+  );
 }
+
+const foodILike = [
+  {
+    name: '돈가스',
+    desc: '빵가루 묻혀서 튀긴 돼지고기',
+  },
+  {
+    name: '치킨',
+    desc: '치느님',
+  },
+  {
+    name: '피자',
+    desc: '혈관 꽉꽉 밀가루 반죽 ',
+  },
+];
 
 function App() {
   return (
     <div>
-      <h1>Hello</h1>
-      <Food fav='kimchi' />
-      <Food fav='라면' />
-      <Food fav='삼겹살' />
-      <Food fav='쭈꾸미' />
+      {foodILike.map((current) => (
+        <Food name={current.name} desc={current.desc} />
+      ))}
     </div>
   );
 }
