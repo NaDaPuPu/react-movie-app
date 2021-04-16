b8f0dc65b904667cde56f1e3bb5bce832d56c2b2
NaDaPuPu <jun19998@gmail.com>
2021-04-17 03:43:14 +0900

    Chapter 2.4.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function Food({ name, image }) {
+function Food({ name, image, rating }) {
   return (
     <div>
       <h2>I like {name}</h2>
+      <h4>{rating}/5.0</h4>
       <img src={image} alt={name} />
     </div>
   );
 }
+
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired,
+};
 
 const foodILike = [
   {
@@ -15,18 +23,21 @@ const foodILike = [
     name: '김치',
     image:
       'http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg',
+    rating: 5.0,
   },
   {
     id: 2,
     name: '삼겹살',
     image:
       'https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg',
+    rating: 4.9,
   },
   {
     id: 3,
     name: '비빔밥',
     image:
       'http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb',
+    rating: 4.5,
   },
 ];
 
@@ -34,7 +45,12 @@ function App() {
   return (
     <div>
       {foodILike.map((food) => (
-        <Food key={food.id} name={food.name} image={food.image} />
+        <Food
+          key={food.id}
+          name={food.name}
+          image={food.image}
+          rating={food.rating}
+        />
       ))}
     </div>
   );
