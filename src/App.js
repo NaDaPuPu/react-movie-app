c3104f814cefff6a4374199dc7672bc1634cc5e5
NaDaPuPu <jun19998@gmail.com>
2021-04-19 18:23:22 +0900

    Chapter 3.2.

--- src/App.js
+++ src/App.js
@@ -1,59 +1,39 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function Food({ name, image, rating }) {
-  return (
-    <div>
-      <h2>I like {name}</h2>
-      <h4>{rating}/5.0</h4>
-      <img src={image} alt={name} />
-    </div>
-  );
-}
-
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  image: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired,
-};
-
-const foodILike = [
-  {
-    id: 1,
-    name: '김치',
-    image:
-      'http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg',
-    rating: 5.0,
-  },
-  {
-    id: 2,
-    name: '삼겹살',
-    image:
-      'https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg',
-    rating: 4.9,
-  },
-  {
-    id: 3,
-    name: '비빔밥',
-    image:
-      'http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb',
-    rating: 4.5,
-  },
-];
-
-function App() {
-  return (
-    <div>
-      {foodILike.map((food) => (
-        <Food
-          key={food.id}
-          name={food.name}
-          image={food.image}
-          rating={food.rating}
-        />
-      ))}
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log('constructor');
+  }
+  state = {
+    count: 0,
+  };
+  add = () => {
+    this.setState((current) => ({ count: current.count + 1 }));
+  };
+  minus = () => {
+    this.setState((current) => ({ count: current.count - 1 }));
+  };
+  componentDidMount() {
+    console.log('component rendered');
+  }
+  componentDidUpdate() {
+    console.log('I just updated');
+  }
+  componentWillUnmount() {
+    console.log('Good bye cruel world');
+  }
+  render() {
+    console.log('render');
+    return (
+      <div>
+        <h1>The number is : {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
+  }
 }
 
 export default App;
